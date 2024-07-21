517c9637855cc2022127e6d918c6fca06d712e7f
rishabhbizzle <rishabhbizzle@gmail.com>
2024-07-21 16:49:25 +0530

    feat: light and dark themes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import ChatBot from './components/ChatBot'
-import Sidebar from './components/Sidebar'
+import ChatBot from "./components/ChatBot";
+import Sidebar from "./components/Sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
-
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[180px_1fr] lg:grid-cols-[200px_1fr]">
-    <Sidebar />
-    <div className="flex flex-col">
-      <ChatBot />
-    </div>
-  </div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="grid min-h-screen w-full md:grid-cols-[180px_1fr] lg:grid-cols-[200px_1fr]">
+        <Sidebar />
+        <div className="flex flex-col">
+          <ChatBot />
+        </div>
+      </div>
+      <Toaster />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -21,31 +21,28 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { ModeToggle } from "./ModeToggle";
 
 const Sidebar = () => {
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 justify-between">
           <div className="flex justify-center items-center gap-2">
-
-          <Package2 className="h-6 w-6" />
-          <span className="">Img-BOT</span>
+            <Package2 className="h-6 w-6" />
+            <span className="">Img-BOT</span>
           </div>
+          <ModeToggle />
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4 mt-5">
-           <Button variant="destructive">
-            Clear All Chats
-           </Button>
+            <Button variant="destructive">Clear All Chats</Button>
           </nav>
         </div>
         <div className="mt-auto p-4">
           <Card x-chunk="dashboard-02-chunk-0">
             <CardHeader className="p-2 pt-0 md:p-4">
-              <CardDescription>
-                Made By Rishabh
-              </CardDescription>
+              <CardDescription>Made By Rishabh</CardDescription>
             </CardHeader>
             <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
               <Button size="sm" className="w-full">
